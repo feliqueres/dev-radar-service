8c189a8a7ca94cad810fd902b5bdfe25ad3e45be
Felipe Queres <feliqueres.gomes.queres@gmail.com>
2020-01-20 01:38:17 -0300

    extraindo código pra outro arquivo e novo método para lista e apagar item pelo ID

--- backend/src/Controllers/DevController.js
+++ backend/src/Controllers/DevController.js
@@ -1,34 +1,51 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/ParseStringAsArray");
 
 module.exports = {
+  async index(request, response) {
+    return response.json(await Dev.find());
+  },
+
   async store(request, response) {
     console.log(request.body);
 
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const api_response = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = api_response.data;
-    console.log(name, avatar_url, bio, github_username);
+    if (!dev) {
+      const api_response = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const techsArray = techs.split(",").map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = api_response.data;
+      console.log(name, avatar_url, bio, github_username);
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      const techsArray = parseStringAsArray(techs);
 
-    const dev = await Dev.create({
-      github_username,
-      bio,
-      avatar_url,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
 
+      dev = await Dev.create({
+        github_username,
+        bio,
+        avatar_url,
+        techs: techsArray,
+        location
+      });
+    }
     return response.json(dev);
+  },
+  async destroy(request, response) {
+    const id = request.params.id;
+    await Dev.deleteOne({ _id: id });
+    return response.status(204);
+  },
+  async update(request, response) {},
+  async find(request, response) {
+    return response.json(await Dev.find({ _id: request.params.id }));
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const { Router } = require("express");
-const DevController = require('./Controllers/DevController');
+const DevController = require("./Controllers/DevController");
+const SearchController = require("./Controllers/SearchController");
 
 const routes = Router();
 
 routes.post("/devs/", DevController.store);
+routes.get("/devs/", DevController.index);
+routes.delete("/devs/:id", DevController.destroy);
+routes.get("/devs/:id", DevController.find);
+
+routes.get("/search/", SearchController.index);
 
 module.exports = routes;
