e8ce20c4bb5664ea6c0fe2e885f2351a06ee4da8
Felipe Queres <feliqueres.gomes.queres@gmail.com>
2020-02-09 13:47:33 -0300

    feat(config): Adicionando dependencia 'cors'

--- code/src/index.js
+++ code/src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 // query param: request.query
 // path param: request.params
@@ -9,14 +9,16 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://admin:Rgj6YJhyomtfpnpp@cluster0-aogyt.mongodb.net/semana-omnistack?retryWrites=true&w=majority',{
+mongoose.connect(
+  "mongodb+srv://admin:Rgj6YJhyomtfpnpp@cluster0-aogyt.mongodb.net/semana-omnistack?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+  }
+);
 
-
-app.use(express.json())
+app.use(cors({}));
+app.use(express.json());
 app.use(routes);
-
 
 app.listen(3333);
